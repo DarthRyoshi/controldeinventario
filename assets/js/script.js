eac349f3265b4caac57a2d16a3137012a8401a4e
DarthRyoshi <kiritoalfaro88@gmail.com>
2025-10-01 12:21:14 -0300

    se elimino el js (sin funcionalidad)

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Cambia la URL visible para ocultar index.php
-(function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const action = urlParams.get('action');
-
-    if (action) {
-        history.replaceState(null, '', '/controlinventario/' + action);
-    }
-})();
